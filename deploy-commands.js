4e818a455ea4941665a54389727d22008438e514
GrevGrisk <153389781+GrevGrisk@users.noreply.github.com>
2026-04-30 23:39:47 +0200

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const { REST, Routes } = require("discord.js");
+
+const friendlyEnemy = require("./modules/friendly-enemy");
+const reportIncident = require("./modules/report-incident");
+
+const commands = [
+  ...friendlyEnemy.data.map(command => command.toJSON()),
+  reportIncident.data.toJSON()
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Commands deployed.");
+  } catch (err) {
+    console.error(err);
+  }
+})();
