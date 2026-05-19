7fe73ffd6aeee93b5787b3c24b2450cf9ed84635
GrevGrisk <153389781+GrevGrisk@users.noreply.github.com>
2026-05-19 11:25:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const friendlyEnemy = require("./modules/friendly-enemy");
 const reportIncident = require("./modules/report-incident");
 const tickets = require("./modules/tickets");
 const steamNews = require("./modules/steam_news");
+const pvpReport = require("./modules/pvpReport");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -17,13 +18,19 @@ client.modules.set(reportIncident.name, reportIncident);
 client.modules.set(tickets.name, tickets);
 client.modules.set(steamNews.name, steamNews);
 
-client.once("clientReady", () => {
+client.once("clientReady", async () => {
   console.log(`Logged in as ${client.user.tag}`);
   steamNews.start(client);
+
+  // Kjør denne én gang for å poste PvP Report-panelet i admin-kanalen.
+  // Etterpå kan du kommentere den ut igjen for å unngå duplikater.
+  // await pvpReport.postAdminPanel(client);
 });
 
 client.on("interactionCreate", async interaction => {
   try {
+    await pvpReport.handleInteraction(interaction);
+
     for (const module of client.modules.values()) {
       if (
         interaction.isChatInputCommand() &&
